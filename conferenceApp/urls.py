748e40e03ef0c6f953f126cb911371a882afccbe
LindaOuer <linda.ouerfelli@gmail.com>
2026-04-30 21:28:32 +0200

    Views - links - extends

--- conferenceApp/urls.py
+++ conferenceApp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import name, home, conference_list
+from .views import conference_delete, conference_details, name, home, conference_list
 
 urlpatterns = [
     path("", home, name="conference_home"),
     path("name/<str:n>", name, name="conference_name"),
     path("list/", conference_list, name="conference_list"),
+    path("details/<int:pk>/", conference_details, name="conference_details"),
+    path("delete/<int:pk>/", conference_delete, name="conference_delete"),
 ]
--- conferenceApp/views.py
+++ conferenceApp/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 from .models import Conference
 
 # Create your views here.
 def conference_list(request):
-    list = Conference.objects.all()
-    # SELECT * FROM conference
+    list = Conference.objects.all().order_by('-start_date')
+    # SELECT * FROM conference ORDER BY start_date DESC
     return render(request, 'conferences/conference_list.html', {'conferences': list})
+
+def conference_details(request, pk):
+    try :
+        conference = Conference.objects.get(conference_id=pk)
+        # SELECT * FROM conference WHERE id = pk
+    except Conference.DoesNotExist:
+        # Handle the case where the conference is not found
+        # return render(request, 'conferences/conference_not_found.html', status=404)
+        return HttpResponse("Conference not found", status=404)
+    return render(request, 'conferences/conference_details.html', {'conference': conference})
+
+def conference_delete(request, pk):
+    try:
+        conference = Conference.objects.get(conference_id=pk)
+        conference.delete()
+    # DELETE FROM conference WHERE conference_id = pk
+    except Conference.DoesNotExist:
+        return render(request, 'conferences/conference_not_found.html', status=404)
+    # return HttpResponse("Conference deleted successfully")
+    return redirect('conference_list', message="Conference deleted successfully")
 
 def home(request):
     return render(request, 'conferences/home.html')
